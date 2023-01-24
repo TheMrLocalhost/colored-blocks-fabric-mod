1098dca55139a26b4a4f42132c7599630cbb4ad4
Zak <zak.konie@gmail.com>
2023-01-24 01:13:26 -0600

    Initial commit

---
+++ src/main/java/com/mrlocalhost/coloredstonebricks/ColoredStoneBricks.java
@@ -0,0 +1,14 @@
+package com.mrlocalhost.coloredstonebricks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ColoredStoneBricks implements ModInitializer {
+	public static final String MOD_ID = "colored_stone_bricks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
